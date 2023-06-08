4c5218ff19b1efe0d2b3a410ca154f8755a3b388
tabby400 <tabbynargaret@gmail.com>
2023-06-08 07:45:08 -0700

    arguements passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,13 @@
 if __name__ == "__main__":
     import sys
 
-    argv = sys.argv
     total = len(sys.argv) - 1  # not including script name
 
     if total == 1:
-        print("1 arguement.")
+        print("1 argument:")
     elif total == 0:
         print("0 arguments.")
     else:
-        print("{} arguements:".format(total))
+        print("{} arguments:".format(total))
     for x in range(total):
         print("{}: {}".format(x + 1, sys.argv[x + 1]))

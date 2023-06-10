3c6f542f0c24af630c7f447948ae35d4b35b5e7e
tabby400 <tabbynargaret@gmail.com>
2023-06-10 12:19:18 -0700

    reversed integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    if my_list is None:  #if no list is passed
+    if my_list is None:  # if no list is passed
         return
     for x in reversed(my_list):
         print("{:d}".format(x))

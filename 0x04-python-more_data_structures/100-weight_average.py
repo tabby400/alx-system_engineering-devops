8615e2f5b073829404c938207851fdd8b23b0eab
tabby400 <tabbynargaret@gmail.com>
2023-06-14 09:55:39 -0700

    weighted value of ints

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    def weight_average(my_list=[]):
+        if not my_list:
+            return 0
+
+    top = 0
+    bottom = 0
+
+    for item in my_list:
+        top = top + item[0] * item[1]
+        bottom = bottom + item[1]
+
+    return (top / bottom)

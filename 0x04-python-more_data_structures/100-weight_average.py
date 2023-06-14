4f924fe68e2340f397e89d0447562c205a5e134e
tabby400 <tabbynargaret@gmail.com>
2023-06-14 10:07:26 -0700

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    def weight_average(my_list=[]):
-        if not my_list:
-            return 0
+    if not my_list:
+        return 0
+
     top = 0
     bottom = 0
 

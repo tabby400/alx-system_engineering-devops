7c5875377cd6a52dc0a393bce23f3f22c5d12651
tabby400 <tabbynargaret@gmail.com>
2023-06-26 05:11:57 -0700

    the first x elements

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """function prints the first x elements
+    only integers
+
+    Arguements:
+        my_list[]: list with the elements
+        x: no of elements to be printed
+    Return: total elements printed
+    """
+
+    total = 0
+
+    for p in range(0, x):
+        try:
+            print("{:d}".format(my_list[p]), end="")
+            total = total + 1
+        except(TypeError, ValueError):
+            continue
+    print("")
+    return (total)

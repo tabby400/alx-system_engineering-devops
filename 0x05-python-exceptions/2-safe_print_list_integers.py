5e0097185a5e2ba887cecceca5c0531bac4ae434
tabby400 <tabbynargaret@gmail.com>
2023-06-26 05:21:26 -0700

    first x elements

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -16,7 +16,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[p]), end="")
             total = total + 1
-        except(TypeError, ValueError):
+        except(ValueError, TypeError):
             continue
     print("")
     return (total)

89185181da6aeeaa3ded04ba01825f1a0d9d67b2
tabby400 <tabbynargaret@gmail.com>
2023-07-04 08:47:47 -0700

    bestschool

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
+def magic_string():  # returns number of times
     magic_string.x = getattr(magic_string, 'x', 0) + 1
     return ("BestSchool, " * (magic_string.x - 1) + "BestSchool")

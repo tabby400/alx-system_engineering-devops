896b4c84c287fa2b934de748ba1bf04fe40aaeaf
tabby400 <tabbynargaret@gmail.com>
2023-07-11 10:06:33 -0700

    fixing file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,5 +7,5 @@ import json
 
 def to_json_string(my_obj):
     """function is used to return the JSON representation of
-    an object string."""
+    an object (string)."""
     return (json.dumps(my_obj))  # encoding python object to JSON str

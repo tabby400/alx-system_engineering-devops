612f002aa0880ee7fbd34accfad1bb4a1397141d
tabby400 <tabbynargaret@gmail.com>
2023-07-11 16:29:36 -0700

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Explains more of a pascal triangle function."""
+
+
+def pascal_triangle(n):
+    """is a representation of new trianfle with n size
+
+    Returns:
+    a list of lists representing triangle.
+    """
+
+    if n <= 0:
+        {
+            return []
+        }
+
+    tri_list = [[1]]
+
+    while len(tri_list) != n:
+        cur_row = triangles[-1]
+        new_rw = [1]
+
+        for p in range(len(cur_row) - 1):
+            new_rw.append(cur_row[p] + cur_row[p + 1])
+        new_rw.append(1)
+        tri_list.append(new_rw)
+
+    return (tri_list)

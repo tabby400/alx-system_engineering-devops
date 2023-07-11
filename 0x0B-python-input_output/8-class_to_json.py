517933c3654df3f92566048c26766bdc7f1d93bd
tabby400 <tabbynargaret@gmail.com>
2023-07-11 10:55:33 -0700

    dictionary description

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Definition of a class in python to JSON"""
+
+
+def class_to_json(obj):
+    """a dictionary description with simple data structure."""
+    return (obj.__dict__)

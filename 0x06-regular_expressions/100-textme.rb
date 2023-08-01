c756b70473e8d13b46ba713cfd402607affee05e
tabby400 <tabbynargaret@gmail.com>
2023-08-01 05:31:40 -0700

    sender and receiver phone number

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# This script outputs sender the sender and
+# the receiver phone number and name
+# Shows the flags used
+# /w matches any word char
+
+
+def get_data(text_in)
+
+  the_matches = text_in.scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/)
+  data = the_matches.map { |match| "#{match[1]},#{match[2]}" }
+  data.join(",")
+end
+
+if ARGV.empty?
+  puts "provide the input text."
+else
+  text_in = ARGV[0]
+  output = data_got(text_in)
+  puts output
+end

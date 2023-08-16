00c16b998d6eedb24605dd03797c43b8d92e9a47
tabby400 <tabbynargaret@gmail.com>
2023-08-16 06:00:28 -0700

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- script creating database hbtn_0d_usa and table cities (in the database hbtn_0d_usa) on MySQL server.
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));

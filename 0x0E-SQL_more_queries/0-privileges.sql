804b22592ee5a14c715e41b4e89368e03745676a
tabby400 <tabbynargaret@gmail.com>
2023-08-16 04:59:54 -0700

    grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script lists all privileges of MySQL users user_0d_2 and user_0d_1 on server (in localhost).
+-- script lists all privileges of MySQL users user_0d_1 and user_0d_2 on server (in localhost).
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

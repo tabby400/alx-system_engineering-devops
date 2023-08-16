8ad269ebbe842330cd3f50f806f79492509edc4c
tabby400 <tabbynargaret@gmail.com>
2023-08-16 10:49:47 -0700

    rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,10 @@
+-- the script Lists shows from hbtn_0d_tvshows_rate by rating.
+-- and sorted in descending order
+
+
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS show
+       INNER JOIN `tv_show_ratings` AS rate
+       ON show.`id` = rate.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DES

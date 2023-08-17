f829ff59543396902aa6ca7514606eb6d0befcfd
tabby400 <tabbynargaret@gmail.com>
2023-08-17 00:49:06 -0700

    all

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,25 +2,19 @@
 -- are not linked to Dexter show sorted in ascending
 
 SELECT DISTINCT `name`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s
-	ON g.`id` = s.`genre_id`
-
-INNER JOIN `tv_shows` AS t
-	ON s.`show_id` = t.`id`
-WHERE t.`title` = "Dexter";
-
-
-SELECT DISTINCT `name`
-FROM `tv_genres` AS g
-WHERE g.`name` NOT IN (
-	SELECT `name`
 	FROM `tv_genres` AS g
-	INNER JOIN `tv_show_genres` AS s
-		ON g.`id` = s.`genre_id`
-	INNER JOIN `tv_shows` AS t
+		INNER JOIN `tv_show_genres` AS s
+		ON g.`id` = s.`genre_id
+
+		INNER JOIN `tv_shows` AS t
 		ON s.`show_id` = t.`id`
-	WHERE t.`title` = "Dexter"
-)
+		WHERE g.`name` NOT IN
+			(SELECT `name`
+				FROM `tv_genres` AS g
+					INNER JOIN `tv_show_genres` AS s
+					ON g.`id` = s.`genre_id`
+
+					INNER JOIN `tv_shows` AS t
+					ON s.`show_id` = t.`id`
+					WHERE t.`title` = "Dexter")
 ORDER BY g.`name`;
-													 ORDER BY g.`name`;

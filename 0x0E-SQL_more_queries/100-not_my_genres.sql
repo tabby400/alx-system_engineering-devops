a7ab5bf2dbf6068e8aeb92de8cc2df1b13f1750a
tabby400 <tabbynargaret@gmail.com>
2023-08-17 00:36:04 -0700

    all

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,20 +1,26 @@
 -- script lists all genres of database hbtn_0d_tvshows that
 -- are not linked to Dexter show sorted in ascending
+
 SELECT DISTINCT `name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
+FROM `tv_genres` AS g
+INNER JOIN `tv_show_genres` AS s
+	ON g.`id` = s.`genre_id`
 
-       INNER JOIN `tv_shows` AS t
-       ON s.`show_id` = t.`id`
-       WHERE g.`name` NOT IN
-             (SELECT `name`
-		FROM `tv_genres` AS g
-		     INNER JOIN `tv_show_genres` AS s
-		     ON g.`id` = s.`genre_id`
+INNER JOIN `tv_shows` AS t
+	ON s.`show_id` = t.`id`
+WHERE t.`title` = "Dexter";
 
-		     INNER JOIN `tv_shows` AS t
-		     ON s.`show_id` = t.`id`
-		     WHERE t.`title` = "Dexter")
+
+SELECT DISTINCT `name`
+FROM `tv_genres` AS g
+WHERE g.`name` NOT IN (
+	SELECT `name`
+	FROM `tv_genres` AS g
+	INNER JOIN `tv_show_genres` AS s
+		ON g.`id` = s.`genre_id`
+	INNER JOIN `tv_shows` AS t
+		ON s.`show_id` = t.`id`
+	WHERE t.`title` = "Dexter"
+)
 ORDER BY g.`name`;
 													 ORDER BY g.`name`;

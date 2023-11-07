c9250423f3daee712b6cae96b323cf55a6c663d1
tabby400 <tabbymargaret@gmail.com>
2023-11-07 11:35:22 -0800

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,14 +9,11 @@ def top_ten(subreddit):
         titles of the first 10 hot posts in reddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for k in range(10):
-                print(children[k].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")  # if issues with http requests
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for h in range(10):
+            print(posts[h]['data']['title'])
+    else:
+        print(None)

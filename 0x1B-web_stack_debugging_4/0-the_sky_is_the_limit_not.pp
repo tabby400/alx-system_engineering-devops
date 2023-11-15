406766d2791e17c48c045983679ae49b974816e7
tabby400 <tabbymargaret@gmail.com>
2023-11-15 15:26:15 +0000

    more requests passed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# traffic in nginx server is increased
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#restarting Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}

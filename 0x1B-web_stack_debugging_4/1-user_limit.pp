70cb233313ec314ba8fdfd97bf55536cca8820f1
tabby400 <tabbymargaret@gmail.com>
2023-11-15 15:33:59 +0000

    holberton user login

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# holberton user logs in and opens a file with no errors
+
+# Holberton user hard file limit increased
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Holberton user soft file limit increased
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
